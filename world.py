d2973fa70ca6b8447539b9fff5297dfb28724399
FacehuggersInc <hulkchomper@gmail.com>
2023-02-03 00:09:29 -0600

    Re-wrote some of the variable placements and combined a function

--- world.py
+++ world.py
@@ -36,35 +36,30 @@ class Room():
 """)
 
 class Map():
-    DIRECTIONS = ["N","S","W","E"]
-
     def __init__(self):
         self.rooms = []
-        self.existingRooms = []
-        self.roomsToCreate = int
-        self.existingRoomIDs = []
 
-        self.directions = []
-
-    def assign_room_id(self, room):
-        while True:
-            id = random.randint(1, self.roomsToCreate)
-            if not id in self.existingRoomIDs:
-                self.existingRoomIDs.append(id)
-                room.id = id
-                return room
-
-    def create_rooms(self):
-        for x in range(self.roomsToCreate):
+    def create_rooms(self, roomsToCreate, existingRoomIDs):
+        for x in range(roomsToCreate):
             room = Room() #Create New Room
-            room = self.assign_room_id(room) #Give it an ID
+            while True:
+                id = random.randint(1, roomsToCreate)
+                if not id in existingRoomIDs:
+                    existingRoomIDs.append(id)
+                    room.id = id
+                    break
             self.rooms.append(room) #Add room to list
 
     def generate_map(self, roomsToCreate = int):
+        DIRECTIONS = ["N","S","W","E"]
+
+        existingRooms = []
+        existingRoomIDs = []
+        directions = []
+
         #Set amount of rooms then create rooms
         if not roomsToCreate: roomsToCreate = random.randint(5,9)
-        self.roomsToCreate = roomsToCreate
-        self.create_rooms()
+        self.create_rooms(roomsToCreate, existingRoomIDs)
 
         #Find Room ID 1 and make it the starting room
         for room in self.rooms:
@@ -73,7 +68,7 @@ class Map():
                 room.coordinate = [0,0]
                 break
 
-        #Bring Starting Room to front of list so we can start there when assigning rooms to doors ina for loop
+        #Bring Starting Room to front of list so we can start there when assigning rooms to doors in a for loop
         for room in self.rooms:
             if room.starting_room == True:
                 self.rooms.pop(self.rooms.index(room))
@@ -82,7 +77,6 @@ class Map():
 
         #PATHING / CONNECT ROOMS-----
         currentCoord = [0,0]
-        roomsMade = 0
         for room in self.rooms:
             turned = False
             roomIndex = self.rooms.index(room)
@@ -95,12 +89,12 @@ class Map():
                 nextRoomsID = self.rooms[roomIndex + 1].id
 
             #Choose a direction
-            direction = random.choice(self.DIRECTIONS)
+            direction = random.choice(DIRECTIONS)
 
             #Corrections
-            if len(self.directions) > 1:
+            if len(directions) > 1:
                 #I want to turn if weve gone the same way twice
-                if self.directions[-1] == direction:
+                if directions[-1] == direction:
                     match direction:
                         case "N": 
                             direction = random.choice(["W","E"])
@@ -112,9 +106,9 @@ class Map():
                             direction = random.choice(["N","S"])
                     turned = True
 
-            if (len(self.directions) > 1) and turned == False:
+            if (len(directions) > 1) and turned == False:
                 #We dont want rooms going directly back onto themselfs
-                match self.directions[-1]:
+                match directions[-1]:
                     case "N": 
                         direction = random.choice(["N","W","E"]) 
                     case "S": 
@@ -129,72 +123,70 @@ class Map():
                 room.coordinate = currentCoord
                 x = room.coordinate[0]
                 y = room.coordinate[1]
-                if self.directions[-1] == "N":  y += 1
-                elif self.directions[-1] == "S": y -= 1
-                elif self.directions[-1] == "W": x -= 1
-                elif self.directions[-1] == "E": x += 1
+                if directions[-1] == "N":  y += 1
+                elif directions[-1] == "S": y -= 1
+                elif directions[-1] == "W": x -= 1
+                elif directions[-1] == "E": x += 1
                 room.coordinate = [x,y]
                 currentCoord = room.coordinate
 
             #No Overlap
-            if len(self.existingRooms) > 1 and room.starting_room == False: #need more than one room to compare
-                for existingRoom in self.existingRooms: #looping here so we can look at each rooms coordinates
+            if len(existingRooms) > 1 and room.starting_room == False: #need more than one room to compare
+                for existingRoom in existingRooms: #looping here so we can look at each rooms coordinates
                     if existingRoom.coordinate == room.coordinate: #<_ Coords still match, loops when it shouldnt really
                         #Back Up in Coords
                         x = room.coordinate[0]
                         y = room.coordinate[1]
-                        if self.directions[-1] == "N":  y -= 1
-                        elif self.directions[-1] == "S": y += 1
-                        elif self.directions[-1] == "W": x += 1
-                        elif self.directions[-1] == "E": x -= 1
+                        if directions[-1] == "N":  y -= 1
+                        elif directions[-1] == "S": y += 1
+                        elif directions[-1] == "W": x += 1
+                        elif directions[-1] == "E": x -= 1
                         room.coordinate = [x,y]
 
                         #Choose a differnt Direction
-                        if self.directions[-1] == "N": 
+                        if directions[-1] == "N": 
                             direction = random.choice(["S","W","E"])
-                        elif self.directions[-1] == "S":
+                        elif directions[-1] == "S":
                             direction = random.choice(["N","W","E"])
-                        elif self.directions[-1] == "W":
+                        elif directions[-1] == "W":
                             direction = random.choice(["S","N","E"])
-                        elif self.directions[-1] == "E":
+                        elif directions[-1] == "E":
                             direction = random.choice(["S","N","W"])
                         
                         #Re Assign Coord
                         x = room.coordinate[0]
                         y = room.coordinate[1]
-                        if self.directions[-1] == "N":  y += 1
-                        elif self.directions[-1] == "S": y -= 1
-                        elif self.directions[-1] == "W": x -= 1
-                        elif self.directions[-1] == "E": x += 1
+                        if directions[-1] == "N":  y += 1
+                        elif directions[-1] == "S": y -= 1
+                        elif directions[-1] == "W": x -= 1
+                        elif directions[-1] == "E": x += 1
                         room.coordinate = [x,y]
                         currentCoord = room.coordinate
-                        
+
                         #break the for loop
                         break
 
             #Assign | Room IDS & Add to directions
             if direction == "N":
                 room.N = nextRoomsID
-                self.directions.append("N")
+                directions.append("N")
             elif direction == "S":
                 room.S = nextRoomsID
-                self.directions.append("S")
+                directions.append("S")
             elif direction == "W":
                 room.W = nextRoomsID
-                self.directions.append("W")
+                directions.append("W")
             elif direction == "E":
                 room.E = nextRoomsID
-                self.directions.append("E")
+                directions.append("E")
+            existingRooms.append(room)
 
-            self.existingRooms.append(room)
-            roomsMade += 1
 
         #Dubuging
         print("\n-- ROOMS -----\n")
         for room in self.rooms:
             print(f"Rooms Index: {self.rooms.index(room)}:{room}")
-        print(f"\nRooms to Make (var): {self.roomsToCreate}")
+        print(f"\nRooms to Make (var): {roomsToCreate}")
         print(f"Blank Rooms Created (objs in list): {len(self.rooms)}")
-        print(f"Rooms Made/Connected: {roomsMade}")
-        print(f"Directions: {self.directions}")
+        print(f"Directions: {directions}")
         hold()
