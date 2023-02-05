25e6d6189b7f27568631406084ce2b6fcda2fee3
FacehuggersInc <hulkchomper@gmail.com>
2023-02-04 23:47:29 -0600

    Chnaging from map_generation to world_gen. Trying a difernt approach to making a world/map

---
+++ world_gen.py
@@ -0,0 +1,29 @@
+import random
+from functions import *
+
+worlds = "A:\PY\Projects\TA\worlds"
+
+class World():
+    def __init__(self):
+        world = []
+        start = (0,0)
+
+    def __str__(self):
+        return(f"""{self.world}""")
+
+    def create_blank_world(self, width, height):
+        saved_worlds = os.listdir(worlds)
+        with open(f"{worlds}\world{(len(saved_worlds) - 1) + 1}.txt","x") as w:
+            for y in range(height):
+                for x in range(width):
+                    w.write("X")
+                w.write("\n")
+
+
+    def generate(self):
+        self.create_blank_world(20,20)
+
+
+if __name__ == "__main__":
+    map = World()
+    map.generate()
