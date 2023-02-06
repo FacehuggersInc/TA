d6fb31a74b94fe3819f2ab381e2489ad6d50b550
FacehuggersInc <hulkchomper@gmail.com>
2023-02-06 01:39:17 -0600

    + Added: functionality to make and load world files - incomplete

--- world_gen.py
+++ world_gen.py
@@ -1,29 +1,58 @@
 import random
 from functions import *
 
-worlds = "A:\PY\Projects\TA\worlds"
+worlds = "worlds\\"
+saved_worlds = os.listdir(worlds)
 
 class World():
     def __init__(self):
-        world = []
-        start = (0,0)
+        self.world_name = None
+        self.world_data = []
+        self.start = (0,0)
 
-    def __str__(self):
-        return(f"""{self.world}""")
+    def load_world(self, world = str):
+        """Loads the tile data from a world file, into a instatiated "World" class"""
+        name = world.split(".")[0]
+        self.world_name = name.split("\\\\")[1]
+        world_data = []
+        with open(f"{world}","r") as w:
+            tile_rows = w.readlines()
+        for y in range(len(tile_rows)):
+            tile_columns = tile_rows[y].split(".")
+            tile_columns[-1] = tile_columns[-1].replace("\n","") #remove the new line
+            world_data.append(tile_columns)
+        self.world_data = world_data
 
-    def create_blank_world(self, width, height):
-        saved_worlds = os.listdir(worlds)
-        with open(f"{worlds}\world{(len(saved_worlds) - 1) + 1}.txt","x") as w:
+    def create_blank_world(self, name = str, size = [int, int]):
+        """Creates a world file of a chosen size, with blank tiles. Returns the world file name."""
+        width = size[0]
+        height = size[1]
+        i = 1
+        if saved_worlds:
+            for saved_world in saved_worlds:
+                if name == saved_world.split(".")[0]: 
+                    new_world = f"{worlds}\{name}{(len(saved_worlds) - 1) + 1}.txt"
+                    break
+                elif i == len(saved_worlds): 
+                    new_world = f"{worlds}\{name}.txt"
+                i += 1
+        else: new_world = f"{worlds}\{name}.txt"
+        self.world_name = new_world
+        with open(f"{new_world}","x") as w:
             for y in range(height):
                 for x in range(width):
-                    w.write("X")
-                w.write("\n")
+                    if x + 1 == width: w.write("[]")
+                    else: w.write("[].")
+                if y + 1 == height:pass
+                else: w.write("\n")
+        return new_world
 
-
-    def generate(self):
-        self.create_blank_world(20,20)
-
+    def generate(self, name, size):
+        """Generate a new world, filled with random tiles."""
+        new_world = self.create_blank_world(name, size)
+        self.load_world(new_world)
 
 if __name__ == "__main__":
     map = World()
-    map.generate()
+    map.generate([11,11])
+    input()
